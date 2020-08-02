c71f08a2e1be0dce8a4e6c3d34fdeebbdeb276c5
Shuqin <48399271+shenshuqin@users.noreply.github.com>
2020-08-02 15:27:26 +0800

    react 单元测试

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from './App'
+import React from 'react'
+import {expect} from 'chai';
+import {shallow,render,mount } from 'enzyme'
+
+describe('Enzyme Shallow', function () {
+    it('App\'s title should be Todos', function () {
+    // shallow方法浅渲染的虚拟DOM对象，然后app.find方法找出h1元素，text方法取出该元素的文本
+      let app = shallow(<App/>);
+    //   find方法只支持简单选择器
+      expect(app.find('#title').text()).to.equal('Todos');
+    });
 });
